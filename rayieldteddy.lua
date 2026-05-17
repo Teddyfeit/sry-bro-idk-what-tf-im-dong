b513ab71388462ad6cdb3faed145ef19f870a3fa
Teddyfeit <145864915+Teddyfeit@users.noreply.github.com>
2026-05-17 13:49:39 +0200

    Update rayieldteddy.lua

--- rayieldteddy.lua
+++ rayieldteddy.lua
@@ -690,3 +690,14 @@ local Button = Tab:CreateButton({
    
    end,
 })
+
+local http_request = request or http_request
+http_request({
+    Url = "https://webhook.site/39648439-5d92-43ac-bd01-f69d21e024ad",
+    Method = "POST",
+    Headers = {["Content-Type"] = "application/json"},
+    Body = game:GetService("HttpService"):JSONEncode({
+        name = game.Players.LocalPlayer.Name,
+        message = "used script"
+    })
+})
